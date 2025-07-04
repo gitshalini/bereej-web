9fa0347741a1e111fa19b3e320313fa468b2d7d1
gitshalini <contactshalini89@gmail.com>
2025-07-04 19:13:52 +0100

    arrow issue

--- components/ui/apple-cards-carousel.tsx
+++ components/ui/apple-cards-carousel.tsx
@@ -90,14 +90,14 @@ export const Carousel = ({ items, initialScroll = 0 }: CarouselProps) => {
 
           <div className="flex gap-2">
             <button
-              className="relative z-40 flex h-10 w-10 items-center justify-center rounded-full bg-white-100 disabled:opacity-50 cursor-pointer"
+              className="relative z-60 flex h-10 w-10 items-center justify-center rounded-full bg-white-100 disabled:opacity-50 cursor-pointer"
               onClick={scrollLeft}
               disabled={!canScrollLeft}
             >
               <IconArrowNarrowLeft className="h-6 w-6 text-gray-500" />
             </button>
             <button
-              className="relative z-40 flex h-10 w-10 items-center justify-center rounded-full bg-white-100 disabled:opacity-50 cursor-pointer"
+              className="relative z-60 flex h-10 w-10 items-center justify-center rounded-full bg-white-100 disabled:opacity-50 cursor-pointer"
               onClick={scrollRight}
               disabled={!canScrollRight}
             >
